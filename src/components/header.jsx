73757ed789d2b8c5bc26984ad7549d8615c2987b
rikumiyao <rikumiyao@gmail.com>
2019-03-07 00:38:27 -0800

    Fixed Navbar issue on mobile

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,23 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class Header extends Component {
   render() {
     const pageURI = window.location.pathname;
+    const component = pageURI.split('/')[1];
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink to="#" className="navbar-brand">YAYtears</NavLink>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <NavLink to="/conquest" className="nav-item nav-link">
-              {(pageURI==='/conquest') ? (<span className="sr-only">(current)</span>) : ''}Conquest
-            </NavLink>
-            <NavLink to="/specialist" className="nav-item nav-link">
-              {(pageURI==='/specialist') ? (<span className="sr-only">(current)</span>) : ''}Specialist
-            </NavLink>
-            <NavLink to="/battlefy" className="nav-item nav-link">
-              {(pageURI==='/battlefy') ? (<span className="sr-only">(current)</span>) : ''}Battlefy
-            </NavLink>
-          </div>
-        </div>
-      </nav>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/conquest" className={"nav-item nav-link"+(component==="conquest"?" active":"")}><NavItem>Conquest</NavItem></Link>
+            <Link to="/specialist" className={"nav-item nav-link"+(component==="specialist"?" active":"")}><NavItem>Specialist</NavItem></Link>
+            <Link to="/battlefy" className={"nav-item nav-link"+(component==="battlefy"?" active":"")}><NavItem>Battlefy</NavItem></Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 }
