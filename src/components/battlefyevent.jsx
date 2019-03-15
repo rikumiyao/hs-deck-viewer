c5fdb7f502c771d355e56486bc651504ecedcad2
rikumiyao <rikumiyao@gmail.com>
2019-03-14 21:35:21 -0700

    added check to see if json file exists

--- src/components/battlefyevent.jsx
+++ src/components/battlefyevent.jsx
@@ -126,8 +126,10 @@ class BattlefyEvent extends Component {
 
   render() {
     let data;
-    if (this.state.id) {
+    try {
       data = require(`../resources/tournamentData/${this.state.id}.json`);
+    } catch (e) {
+
     }
     if (this.state.isLoaded && !this.state.error && this.state.bracketStarted) {
       return (
@@ -155,7 +157,7 @@ class BattlefyEvent extends Component {
                     <tr key={name}>
                       <td>{value['position']+1}</td>
                       <td><Link to={`/battlefy/${this.state.id}/${value['matchId']}?player=${encodeURIComponent(name)}`}>{name}</Link></td>
-                      <td>{data[name][0].toUpperCase()+data[name].substring(1).toLowerCase()}</td>
+                      <td>{data ? data[name][0].toUpperCase()+data[name].substring(1).toLowerCase() : ""}</td>
                       <td>{value['wins']+"-"+value['losses']}</td>
                       <td>{value['place']}</td>
                     </tr>);
