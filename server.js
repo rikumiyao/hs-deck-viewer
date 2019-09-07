daa0c3b1bf695db10320c311e61a8efdfa0c758b
rikumiyao <rikumiyao@gmail.com>
2019-09-07 16:10:33 -0700

    test meta tags

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, './build')));
+
+app.get('*', function(request, response) {
+  const filePath = path.resolve(__dirname, './build', 'index.html');
+  response.sendFile(filePath);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
