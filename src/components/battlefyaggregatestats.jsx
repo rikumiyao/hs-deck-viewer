aadc54079431b0f2cb7eac9d96da2970e4972c9d
Riku Miyao <rikumiyao@arista.com>
2022-05-19 14:31:20 -0700

    overdue update for masters tour 5

--- src/components/battlefyaggregatestats.jsx
+++ src/components/battlefyaggregatestats.jsx
@@ -7,7 +7,7 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 class BattlefyTop8 extends Component {
 
-  tournaments = ['four', 'three', 'two', 'one', 'undercity','stormwind', 'silvermoon'];
+  tournaments = ['five', 'four', 'three', 'two', 'one', 'undercity','stormwind', 'silvermoon'];
 
   state = {
     eventLocation: this.tournaments[0],
